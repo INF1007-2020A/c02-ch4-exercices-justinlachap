d69facc4c44faa453793b918ee6f694cb43a615e
Justin Lachapelle <46169956+justinlachap@users.noreply.github.com>
2020-09-17 17:28:16 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,36 +1,52 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import random
 
 def is_even_len(string):
-	return 0xDEADBEEF
+    if len(string)%2==0:
+        return True
+    else:
+        return False
 
 
 def get_num_char(string, char):
-	return 0xDEADBABE
+    counter = 0
+    for i in string:
+        if i==char:
+            counter += 1
+    return counter
 
 
 def get_first_part_of_name(name):
-	return ""
+    s = ''
+    for i in name:
+        if i == '-':
+            break
+        s+= i
+    s=s.capitalize()
+    return s
 
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+    animals_list = [str(i) for i in animals]
+    adjectives_list = [str(i) for i in adjectives]
+    fruits_list = [str(i) for i in fruits]
+    return 'Aujourd’hui, j’ai vu un ' + random.choice(animals_list)+ ' s’emparer d’un panier '+ random.choice(adjectives)+ ' plein de '+random.choice(fruits)+'.'
 
 
 if __name__ == "__main__":
-	spam = "Bonjour!"
-	parity = "pair" if is_even_len(spam) else "impair"
-	print(f"Le nombre de caractère dans la chaine '{spam}' est {parity}.")
+    spam = "Bonjour!"
+    if is_even_len(spam)==True:
+        parity = 'pair'
+    else:
+        parity = "impair"
+    print(f"Le nombre de caractère dans la chaine '{spam}' est {parity}.")
 
-	eggs = "Hello, world!"
-	print(f"Le nombre d'occurrence de l dans '{eggs}' est : {get_num_char(eggs, 'l')}.")
+    eggs = "Hello, world!"
+    print(f"Le nombre d'occurrence de l dans '{eggs}' est : {get_num_char(eggs, 'l')}.")
 
-	parrot = "jean-marc"
-	print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
+    parrot = "jean-marc"
+    print('Bonjour ' + get_first_part_of_name(parrot))
 
-	animals = ("chevreuil", "chien", "pigeon")
-	adjectives = ("rouge", "officiel", "lourd")
-	fruits = ("pommes", "kiwis", "bananes")
-	print(get_random_sentence(animals, adjectives, fruits))
+    animals = ("chevreuil", "chien", "pigeon")
+    adjectives = ("rouge", "officiel", "lourd")
+    fruits = ("pommes", "kiwis", "bananes")
+    print(get_random_sentence(animals, adjectives, fruits))
